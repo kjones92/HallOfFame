c567a5a216418a77b6e2dadac435bf87898c06de
Katrina Jones <katrina.jones92@googlemail.com>
2022-02-13 16:08:40 +0000

    Added post functionality to hall of fame

--- README.md
+++ README.md
@@ -11,21 +11,32 @@ We use the following technologies:
 In order to get MySql running and for flyway to run we use the command command while in the root hall of fame directory:
 
 ```
-docker-compose up
+docker compose up
 ```
 
 In the event that we need to clear down the database from testing content or to make changes to the tables the follow steps should be followed:
 
 1. Navigate to the root Hall Of Fame directory
-2. Run `docker-compose down`
+2. Run `docker compose down`
 3. Run `docker volume prune --force`
-4. Run `docker-compose up`
+4. Run `docker compose up`
 
 # Web
 This application is written in php and uses `atk4/ui` to serve the website. To run the application simply navigate into the server directory and type:
 
 ```
-php -S 127.0.0.1:8080
+php -S localhost:8080
 ```
 
 Once this has been you, you can navigate to the website at: http://localhost:8080/
+
+
+
+#Step by Step
+
+1. Open Terminal - iTerm
+2. Navigate into HallOfFame - `cd Desktop/HallOfFame`
+3. Execute `docker compose up`
+3. Open another tab in terminal - Menu > Shell > Add Tab
+4. Navigate new tab into Server - `cd Desktop/HallOfFame/Server`
+5. Execute - `php -S localhost:8080`
--- server/index.php
+++ server/index.php
@@ -1,4 +1,14 @@
 <?php include("includes/db_conn.php"); ?>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $newtitle = $_REQUEST['title'];
+  $newYear = $_REQUEST['year'];
+
+  $sql = "INSERT INTO album(title, year)  VALUES ('$newtitle', '$newYear')";
+  mysqli_query($dbConn, $sql);
+}
+?>
 <!DOCTYPE html>
 
 <html>
@@ -6,7 +16,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PHP Template Example</title>
+  <title>Hall of Fame</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
 </head>
 
@@ -15,9 +25,9 @@
   <div class="container">
     <h1 class="title">
       <?php
-        if ($dbCheck == false ) {
-          echo "Cannot connect to database";
-        }
+      if ($dbCheck == false) {
+        echo "Cannot connect to database";
+      }
       ?>
     </h1>
 
@@ -39,6 +49,20 @@
     }
 
     ?>
+
+    
+
+    <form action=index.php method="POST">
+      <p>
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title">
+      </p>
+      <p>
+        <label for="year">Year:</label>
+        <input type="text" name="year" id="year">
+      </p>
+      <input type="submit" value="Submit">
+    </form>
 
   </div>
 
